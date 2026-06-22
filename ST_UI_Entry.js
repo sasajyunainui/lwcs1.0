e83c58b98e6cd9b73d905581adac67b631136047
sasajyunainui <sagiriruby0921@gmail.com>
2026-06-22 20:40:14 +0900

    up

--- ST_UI_Entry.js
+++ ST_UI_Entry.js
@@ -13,7 +13,7 @@
   const 调试热更新模式 = !!宿主窗口[加载器键];
   宿主窗口[加载器键] = true;
 
-  const 默认资源基础地址 = 'https://testingcf.jsdelivr.net/gh/sasajyunainui/lwcs1.0@bf0218a6c40d25b246b46aa2a52a7482f9e1971b/';
+  const 默认资源基础地址 = 'http://localhost:5502/lwcs/';
   const 资源基础地址 = (() => {
     const 覆盖地址 = String(宿主窗口.__LWCS_资源基础地址__ || window.__LWCS_资源基础地址__ || '').trim();
     if (!覆盖地址) return 默认资源基础地址;
