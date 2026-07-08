07ee87785e2127c4504bbef7f889f92c4882ef41
sajyunainui <sagiriruby0921@gmail.com>
2026-07-09 00:13:11 +0900

    Update request monitor performance build hash

--- ST_UI_Entry.js
+++ ST_UI_Entry.js
@@ -13,7 +13,7 @@
   const 调试热更新模式 = !!宿主窗口[加载器键];
   宿主窗口[加载器键] = true;
 
-  const 默认资源基础地址 = 'https://testingcf.jsdelivr.net/gh/sasajyunainui/lwcs1.0@5debae6f4ce5aeb1129cfefa9570b56b82bc07e4/';
+  const 默认资源基础地址 = 'https://testingcf.jsdelivr.net/gh/sasajyunainui/lwcs1.0@095bf8304d47c6a8eee8e828b465ffe3869481ab/';
   const 资源基础地址 = (() => {
     const 覆盖地址 = String(宿主窗口.__LWCS_资源基础地址__ || window.__LWCS_资源基础地址__ || '').trim();
     if (!覆盖地址) return 默认资源基础地址;
