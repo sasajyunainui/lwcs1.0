523e2d36da195540fd9ed8e2cbfab6ab8049703a
sajyunainui <sagiriruby0921@gmail.com>
2026-07-09 12:48:26 +0900

    Point entry to dropdown filter update

--- ST_UI_Entry.js
+++ ST_UI_Entry.js
@@ -13,7 +13,7 @@
   const 调试热更新模式 = !!宿主窗口[加载器键];
   宿主窗口[加载器键] = true;
 
-  const 默认资源基础地址 = 'https://testingcf.jsdelivr.net/gh/sasajyunainui/lwcs1.0@fca38c1af096ed9867e6949b5e3313402cc95819/';
+  const 默认资源基础地址 = 'https://testingcf.jsdelivr.net/gh/sasajyunainui/lwcs1.0@757cd3c33406dfeeef8af49eb51ec717aac0f941/';
   const 资源基础地址 = (() => {
     const 覆盖地址 = String(宿主窗口.__LWCS_资源基础地址__ || window.__LWCS_资源基础地址__ || '').trim();
     if (!覆盖地址) return 默认资源基础地址;
