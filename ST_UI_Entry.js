899a468d449efae054aa7f3b76c6ecf6aca1f279
sajyunainui <sagiriruby0921@gmail.com>
2026-07-09 15:42:06 +0900

    Point entry to turn floor labels

--- ST_UI_Entry.js
+++ ST_UI_Entry.js
@@ -13,7 +13,7 @@
   const 调试热更新模式 = !!宿主窗口[加载器键];
   宿主窗口[加载器键] = true;
 
-  const 默认资源基础地址 = 'https://testingcf.jsdelivr.net/gh/sasajyunainui/lwcs1.0@b23405cb2e6623cfe83798d841695f56e54b1297/';
+  const 默认资源基础地址 = 'https://testingcf.jsdelivr.net/gh/sasajyunainui/lwcs1.0@aacc0a78e42afb37c9688af13bb5b4248a22bb0e/';
   const 资源基础地址 = (() => {
     const 覆盖地址 = String(宿主窗口.__LWCS_资源基础地址__ || window.__LWCS_资源基础地址__ || '').trim();
     if (!覆盖地址) return 默认资源基础地址;
