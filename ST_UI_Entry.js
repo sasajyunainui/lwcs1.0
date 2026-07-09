60d3477508db506909c71dd8fd00659cb54143e3
sajyunainui <sagiriruby0921@gmail.com>
2026-07-09 12:24:04 +0900

    Point entry to request monitor search scope

--- ST_UI_Entry.js
+++ ST_UI_Entry.js
@@ -13,7 +13,7 @@
   const 调试热更新模式 = !!宿主窗口[加载器键];
   宿主窗口[加载器键] = true;
 
-  const 默认资源基础地址 = 'https://testingcf.jsdelivr.net/gh/sasajyunainui/lwcs1.0@fc982fcbc807739e4ead393b47a49867aa954406/';
+  const 默认资源基础地址 = 'https://testingcf.jsdelivr.net/gh/sasajyunainui/lwcs1.0@2bc09bd54df648a12975cfb7361a0da6c4a33b80/';
   const 资源基础地址 = (() => {
     const 覆盖地址 = String(宿主窗口.__LWCS_资源基础地址__ || window.__LWCS_资源基础地址__ || '').trim();
     if (!覆盖地址) return 默认资源基础地址;
