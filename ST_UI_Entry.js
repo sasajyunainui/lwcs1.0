13c1d30569e659d87955360e5cb94cc4cea994d6
sajyunainui <sagiriruby0921@gmail.com>
2026-07-12 23:40:41 +0900

    chore(lwcs): update request monitor resource

--- ST_UI_Entry.js
+++ ST_UI_Entry.js
@@ -13,7 +13,7 @@
   const 调试热更新模式 = !!宿主窗口[加载器键];
   宿主窗口[加载器键] = true;
 
-  const 默认资源基础地址 = 'https://testingcf.jsdelivr.net/gh/sasajyunainui/lwcs1.0@79417c4336793e7e36b2eeafab6239529f51f038/';
+  const 默认资源基础地址 = 'https://testingcf.jsdelivr.net/gh/sasajyunainui/lwcs1.0@3fa182afcc8ca2daf2d300b79b120eca26193123/';
   const 资源基础地址 = (() => {
     const 覆盖地址 = String(宿主窗口.__LWCS_资源基础地址__ || window.__LWCS_资源基础地址__ || '').trim();
     if (!覆盖地址) return 默认资源基础地址;
