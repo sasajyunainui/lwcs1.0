4514bcacf84edb1ccdec6d6805ebe1c135d898c9
sajyunainui <sagiriruby0921@gmail.com>
2026-07-14 23:15:32 +0900

    chore(loader): load reload-safe request monitor

--- ST_UI_Entry.js
+++ ST_UI_Entry.js
@@ -13,7 +13,7 @@
   const 调试热更新模式 = !!宿主窗口[加载器键];
   宿主窗口[加载器键] = true;
 
-  const 默认资源基础地址 = 'https://testingcf.jsdelivr.net/gh/sasajyunainui/lwcs1.0@0e77e42/';
+  const 默认资源基础地址 = 'https://testingcf.jsdelivr.net/gh/sasajyunainui/lwcs1.0@bd8a168/';
   const 资源基础地址 = (() => {
     const 覆盖地址 = String(宿主窗口.__LWCS_资源基础地址__ || window.__LWCS_资源基础地址__ || '').trim();
     if (!覆盖地址) return 默认资源基础地址;
