da016ac0b90694f28d21720815fa0a0c1dea59a2
sajyunainui <sagiriruby0921@gmail.com>
2026-07-16 18:09:57 +0900

    point loader to compact toolbar fix

--- ST_UI_Entry.js
+++ ST_UI_Entry.js
@@ -13,7 +13,7 @@
   const 调试热更新模式 = !!宿主窗口[加载器键];
   宿主窗口[加载器键] = true;
 
-  const 默认资源基础地址 = 'https://testingcf.jsdelivr.net/gh/sasajyunainui/lwcs1.0@851411fbb2f22c27a6e497e642d76539ebddbb0a/';
+  const 默认资源基础地址 = 'https://testingcf.jsdelivr.net/gh/sasajyunainui/lwcs1.0@7517226466b5018c45c0b44cb7023f07e05a12a5/';
   const 资源基础地址 = (() => {
     const 覆盖地址 = String(宿主窗口.__LWCS_资源基础地址__ || window.__LWCS_资源基础地址__ || '').trim();
     if (!覆盖地址) return 默认资源基础地址;
