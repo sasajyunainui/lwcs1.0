b46a90f553cfb04ad17289ff5b217114c92918ee
sajyunainui <sagiriruby0921@gmail.com>
2026-08-30 00:03:18 +0900

    收紧 MVU 初始化提交边界

--- MVU_ZOD_Entry.js
+++ MVU_ZOD_Entry.js
@@ -18,7 +18,7 @@ const MVU_UI_PREFETCH_FILES_V1 = Object.freeze([
   'sheep_map_restore.js',
 ]);
 const MVU_ENGINE_UPSTREAM_COMMIT_V1 = '0a730cd4a9b99689d1135a49b542c780b977c24c';
-const MVU_ENGINE_BUNDLE_SHA256_V1 = 'b67dab908a70218d0af06d27c6afb80d17ad750c545d8297cd88df107e2ee53a';
+const MVU_ENGINE_BUNDLE_SHA256_V1 = '8826bab2ba578ae340248804d6d4a13f9bbc7b1fcac5674934a08685420810b1';
 const MVU追踪模块顺序_V1 = Object.freeze([
   'MVU_ZOD_Entry.js',
   MVU_ENGINE_BUNDLE_FILE_V1,
